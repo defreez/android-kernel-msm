b830226ca6c6caa3a73ce2cb48b1c1f41f6f37ac
Daniel DeFreez <daniel@defreez.com>
2011-09-25 14:05:02 -0700

    hardcode compile.h

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 71 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#72 PREEMPT Sun Sep 25 10:46:58 PDT 2011"
+#define LINUX_COMPILE_TIME "10:46:58"
+#define LINUX_COMPILE_BY "moxie"
+#define LINUX_COMPILE_HOST "searching"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
